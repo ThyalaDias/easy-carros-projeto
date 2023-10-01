10fdd93800108970934199720ced1b03945bcc39
ThyalaDias <dias.thyala@gmail.com>
2023-10-01 08:10:57 -0300

    final

--- readme.md
+++ readme.md
@@ -1,4 +1,2 @@
-- arrumar caminhos, 
-- arrumar mensagem toast liberacao carro
-- padding gabi
-- menu mobile
+Trabalho Segundo Semetre FIAP
+Desafio BRQ.
--- scripts/pagamento.js
+++ scripts/pagamento.js
@@ -1,16 +1,13 @@
-// Obtém referências para os elementos dos formulários
 const pixForm = document.getElementById("pixForm");
 const cartaoCreditoForm = document.getElementById("cartaoCreditoForm");
 const cartaoDebitoForm = document.getElementById("cartaoDebitoForm");
 
-// Função para alternar entre os formulários com base no tipo de pagamento selecionado
 function alternarFormulario(tipoPagamento) {
     pixForm.style.display = tipoPagamento === "pix" ? "block" : "none";
     cartaoCreditoForm.style.display = tipoPagamento === "cartaoCredito" ? "block" : "none";
     cartaoDebitoForm.style.display = tipoPagamento === "cartaoDebito" ? "block" : "none";
-    }
+}
 
-// Adicione um evento de clique aos rótulos dos métodos de pagamento
 document.querySelector("label[for=pix]").addEventListener("click", () => alternarFormulario("pix"));
 document.querySelector("label[for=cartaoCredito]").addEventListener("click", () => alternarFormulario("cartaoCredito"));
 document.querySelector("label[for=cartaoDebito]").addEventListener("click", () => alternarFormulario("cartaoDebito"));
